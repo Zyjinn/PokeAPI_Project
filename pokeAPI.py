148c253472a32268afe35969f2c71f329d36d388
Zygen <Xsaber64@Gmail.com>
2020-09-01 16:50:30 -0500

    Added base main function and imports and a function to get a pokemon name for the API call

--- pokeAPI.py
+++ pokeAPI.py
@@ -1,8 +1,28 @@
-# Get the pokemon's names, weight, and height and return them within the python console
+# Imports Json and requests (to get API requests)
+import json
+import requests
 
-# TODO: Ask for user input of a certain pokemon?
+# Get the pokemon's names, weight, and height and return them within the python console
+def main():
+    # Get the pokemon's name
+    pokemonName = getPokemonName()
+
+
+# Ask the user for what pokemon's name they want to retrieve
+# given: None
+# returns: pokemon name(taken from user input)
+def getPokemonName(): 
+    print("What Pokemon do you want to retrieve information for?")
+    pokeName = input()
+    return pokeName
 
 # TODO: Create a get request to get the pokemon's name, weight and height from the json
 
+
 # TODO: print and format the data in the json
 
+
+
+# Main function called
+if __name__ == "__main__":
+    main()
