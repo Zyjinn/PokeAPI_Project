bb6afe7f85b689e61b7e9dc2b416ad4a8bd33d61
Zygen <Xsaber64@Gmail.com>
2020-08-31 20:26:19 -0500

    first commit

---
+++ pokeAPI.py
@@ -0,0 +1,8 @@
+# Get the pokemon's names, weight, and height and return them within the python console
+
+# TODO: Ask for user input of a certain pokemon?
+
+# TODO: Create a get request to get the pokemon's name, weight and height from the json
+
+# TODO: print and format the data in the json
+
