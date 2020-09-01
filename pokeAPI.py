e9503a505f8141024502aa8f626a9ba2c31d10f6
Zygen <Xsaber64@Gmail.com>
2020-09-01 17:42:14 -0500

    Created functions to grab information from the pokeapi and return as json dictionary, and then function to print necessary data

--- pokeAPI.py
+++ pokeAPI.py
@@ -7,6 +7,12 @@ def main():
     # Get the pokemon's name
     pokemonName = getPokemonName()
 
+    # Request the pokemon from the API
+    requestDictionary = requestPokeAPI(pokemonName)
+
+    # Print the data for the pokemon
+    printPokeInfo(requestDictionary)
+
 
 # Ask the user for what pokemon's name they want to retrieve
 # given: None
@@ -17,9 +23,49 @@ def getPokemonName():
     return pokeName
 
 # TODO: Create a get request to get the pokemon's name, weight and height from the json
+# Gets the requested pokemon from the API
+# Given, a pokemon name
+# prints the data(name + weight + height) to the console for said pokemon to the console
+def requestPokeAPI(pokeName):
+    print(f'Requesting {pokeName} from the API...')
+    # Create string for request URL pokemon's name
+    requestURL = "https://pokeapi.co/api/v2/pokemon/" + pokeName
+    # Make sure the request is lowercase
+    requestURL = requestURL.casefold()
+    print(requestURL) # TESTING
+
+    # Fetch the data from the URL
+    r = requests.get(requestURL)
+
+    # Get data as a json
+    jsonInfo = r.json
+    # data = json.load(jsonInfo)
+
+    pokeDictionary = r.json()
+    print(f'Request for {pokeName} complete!\n')
+    return pokeDictionary
+
+
 
 
 # TODO: print and format the data in the json
+def printPokeInfo(pokeDictionary):
+    # Easy variables for printing
+    name = pokeDictionary['name'].title()
+
+    weight = pokeDictionary['weight']
+    # Convert hectograms -> kilograms
+    weight = weight/10
+
+    height = pokeDictionary['height']
+    # convert decimeters -> to feet
+    height = height*3.937008
+    heightFt = round(height/12)
+    heightIn = round(height%12)
+
+
+    # Print the information for the specified
+    print(f'Name: {name}\nWeight: {weight}kg\nHeight: {heightFt}ft, {heightIn}in')
 
 
 
